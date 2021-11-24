731da31f1583940649801a18dd644e8865733861
Deveak <tachittanachote.tsch@mail.kmutt.ac.th>
2021-11-24 22:26:52 +0700

    Update Payment

---
+++ gobackend/controllers/walletTransactionController.js
@@ -0,0 +1,45 @@
+const db = require("../models/db")
+
+exports.addWalletTransaction = (wallet) => {
+    return new Promise((resolve, reject) => {
+        db.query(`INSERT INTO wallet_transactions (wallet_transaction_id, user_id, status, amount, type) VALUES ('?', ?, '?', ?, '?')`, 
+            [
+                wallet.id, 
+                wallet.user_id, 
+                wallet.status, 
+                wallet.amount,
+                wallet.type,
+            ], (err, result) => {
+            if (err) reject(err);
+            resolve(result);
+        });
+    });
+}
+
+exports.updateWalletTransactionById = (id, status) => {
+    return new Promise((resolve, reject) => {
+        db.query(`UPDATE wallet_transactions SET status = '?', updated_at = ? WHERE wallet_transaction_id = ?`, [id, status], (err, result) => {
+            if (err) reject(err);
+            resolve(result);
+        });
+    });
+}
+
+
+exports.getWalletTransactionById = (id) => {
+    return new Promise((resolve, reject) => {
+        db.query(`SELECT * FROM wallet_transactions WHERE wallet_transaction_id = '?'`, [id], (err, result) => {
+            if (err) reject(err);
+            resolve(result);
+        });
+    });
+}
+
+exports.getPendingWalletTransactionByUserId = (id) => {
+    return new Promise((resolve, reject) => {
+        db.query(`SELECT * FROM wallet_transactions WHERE user_id = ?`, [id], (err, result) => {
+            if (err) reject(err);
+            resolve(result);
+        });
+    });
+}
---
+++ gofront/screens/payments/CompleteScreen.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import { View } from 'react-native'
+
+class CompleteScreen extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <View>
+
+            </View>
+        )
+    }
+
+}
+
+export default CompleteScreen
