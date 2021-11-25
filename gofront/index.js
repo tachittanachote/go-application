610c7fdfab464823f79b54d35fe3e205233c25ba
Thuvanon <tuvanontopcast@gmail.com>
2021-11-25 19:33:30 +0700

    update HistoryScreen

--- gofront/index.js
+++ gofront/index.js
@@ -8,6 +8,7 @@ import {name as appName} from './app.json';
 
 import axios from 'axios';
 
-axios.defaults.baseURL = 'https://api.gotogetherapp.me/api/v1';
+//axios.defaults.baseURL = 'https://api.gotogetherapp.me/api/v1';
+axios.defaults.baseURL = 'http://192.168.2.124:3000/api/v1';
 
 AppRegistry.registerComponent(appName, () => Main);
