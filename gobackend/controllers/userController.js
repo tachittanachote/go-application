4589ea2630f863226b542967ef4696cd16cce539
maxdemonkung <48761367+maxdemonkung@users.noreply.github.com>
2021-11-28 17:30:06 +0700

    push

--- gobackend/controllers/userController.js
+++ gobackend/controllers/userController.js
@@ -7,12 +7,9 @@ const GET_USER_STATEMENT = "SELECT * FROM users WHERE user_id = ? "
 const GET_USER_STATEMENT_BY_PHONE_NUMBER = "SELECT * FROM users WHERE phone_number = ?"
 const GET_USER_STATEMENT_BY_PHONE_NUMBER_AND_DEVICE_ID = "SELECT * FROM users WHERE phone_number = ? and device_id = ?"
 const UPDATE_USER_STATEMENT_BY_PHONE_NUMBER = "UPDATE users SET device_id = ? WHERE phone_number = ?"
-<<<<<<< HEAD
 const UPDATE_USER_INFORMATION_BY_USER_ID = "UPDATE users SET first_name = ?, last_name = ?, email = ?, phone_number = ?, gender = ?, date_of_birth = ?, emergency_phone_number = ?, license_plate = ?, brand = ?, model = ?, color = ?, driver_license_id = ?, citizen_id = ?  WHERE user_id = ? "
-=======
 const UPDATE_USER_BALANCE_STATEMENT_BY_USER_ID = "UPDATE users SET wallet_balance = ? WHERE user_id = ?"
-const UPDATE_USER_INFORMATION_BY_USER_ID = "UPDATE users SET first_name = ?, last_name = ?, email = ?, phone_number = ?, gender = ?, date_of_birth = ?, emergency_phone_number = ?, license_plate = ?, brand = ?, model = ?, color = ?  WHERE user_id = ? "
->>>>>>> 0fa592a71e4f198828c8fd0303715d36e77720a8
+
 
 exports.addUser = (user) => {
     const id = generateUniqueId({
